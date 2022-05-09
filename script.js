fbdce760b2dd8d32c29ffc061a5b96de76218f61
Kaegan O'Hara <kohara321@gmail.com>
2022-05-09 00:48:02 -0600

    style change, about photo added, animations

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "About Me:";
-document.querySelector("body").appendChild(h2);
+const lastName = document.getElementById('lastName');
+const firstName = document.querySelector('h1');
+
+function colorChange() {
+  lastName.style.color = '#f71924';
+  firstName.style.color = '#f3ff4a';
+}
+
+firstName.addEventListener('mouseover', colorChange);
+lastName.addEventListener('mouseover', colorChange);
+
+function mouseOut () {
+    lastName.style.color = '#9e9e9b';
+    firstName.style.color = '#9e9e9b';
+}
+
+lastName.addEventListener('mouseout', mouseOut);
+firstName.addEventListener('mouseout', mouseOut);
+
+function spin() {
+    const wolf = document.getElementById('wolf');
+    wolf.addEventListener('click', spin);
+};
